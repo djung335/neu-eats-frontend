98825b2b125556ebb08324ad100f0f467d046fe2
Daniel Jung <danieljung335@gmail.com>
2021-11-17 18:38:05 -0500

    browser routing

---
+++ src/components/Home/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+      <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Button
+      </button>
+  );
+
+};
+
+export default Home;
