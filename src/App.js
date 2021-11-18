57d5a7449cc9a4121cbeb774987151b5525275ae
Daniel Jung <danieljung335@gmail.com>
2021-11-18 15:51:47 -0500

    routing

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import './App.css';
 import React from "react";
 import Home from "./components/Home";
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import { ChakraProvider } from "@chakra-ui/react"
+import {ChakraProvider} from "@chakra-ui/react"
+import Search from "./components/Search";
+import Details from "./components/Details";
+import Profile from "./components/Profile";
+import Login from "./components/Login";
 
 
 const App = () => {
   return (
       <ChakraProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+        <BrowserRouter>
+          <Routes>
+            <Route path={"/"} element={<Home/>} />
+            <Route path="/search" element={<Search/>} />
+            <Route path="/details" element={<Details/>} />
+            <Route path="/profile" element={<Profile/>} />
+            <Route path="/login" element={<Login/>} />
+          </Routes>
+        </BrowserRouter>
       </ChakraProvider>
 
   );
---
+++ src/components/Search/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Search = () => {
+  return(<h1>Hello</h1>);
+
+
+};
+
+export default Search;
