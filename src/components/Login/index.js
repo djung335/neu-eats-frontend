221d49001d3b3ca32fee9c16e10821bf28739c59
Daniel Kim <51827691+folaroma@users.noreply.github.com>
2021-11-24 19:27:57 -0500

    portion of login page done

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,20 +1,26 @@
 import React from "react";
-import { Box, Stack, VStack, Flex, Grid, Center, Text, Square } from "@chakra-ui/react";
+import { Box, Stack, HStack, VStack, Flex, Grid, Center, Text, Square, IconButton } from "@chakra-ui/react";
+import { MdFastfood } from "react-icons/all";
 
 const Login = () => {
   return(
     <Flex minH="100vh">
       <Box flex="4" w="full" h="100vh" bg="#aacaef">
         <Flex minH="100vh">
-          <VStack w="full" h="100vh" alignItems="flex-start">
-            <Box w="full" h="10%">
+          <VStack w="full" h="100vh">
+            <Box w="full" h="10%" marginLeft="10%" marginTop="2.5%">
+              <HStack spacing={5}>
+                <IconButton icon={<MdFastfood size={25}/>} w="50px" h="50px" p={3} bg="#7986e6" borderRadius="10px" color="white"/>
+                <Text fontWeight="bold" fontSize="2em" color="#76797d"> NEU Eats </Text>
+              </HStack>
             </Box>
-            <Box w="full" h="90%">
-                <Text> Find good foods faster </Text>
+            <Box w="full" h="90%" display="flex" alignItems="center" justifyContent="center">
+                <Text fontSize="5.3em" fontWeight="600" marginBottom="15%" marginRight="10%" lineHeight="115%" color="white"> Find good food <br /> <u> faster </u> </Text>
             </Box>
           </VStack>
         </Flex>
       </Box>
+      
       <Box flex="3" w="full" h="100vh" bg="white">
         
       </Box>
