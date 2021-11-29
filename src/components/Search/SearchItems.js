2277a4c3088797b42c92d385f1ee41d9ed9f7bd4
nguy3n.nhat <nguy3n.nhat@gmail.com>
2021-11-28 20:10:05 -0500

    nguyen

---
+++ src/components/Search/SearchItems.js
@@ -0,0 +1,68 @@
+import React from "react";
+import "./index.css"
+import {Box, Heading, HStack, Image, Text, VStack} from "@chakra-ui/react";
+import {FaPaw, FaCheck, FaTimes} from "react-icons/all";
+import rest from "./rest.json";
+
+const SearchItem = ({
+  rest = {
+    restIcon: "/images/java-logo.jpeg",
+    restName: "Java", restRating: 4,
+    restReviews: "10", restDes:"Lorem something",
+    studentRev: "10", otherRev: "4",
+    husky: true, discount: false,
+    tags: [ "Japanese", "Sushi", "Bar" ]
+  },
+  index={ }
+}) => {
+  return (
+      <Box p={3} borderWidth={2} shadow="xl" rounded="lg">
+        <HStack w="full">
+          <Box w="30%"><Image src="" alt="Restaurant Photo"/></Box>
+          <Box>
+          </Box>
+          <VStack align="left">
+            <Heading fontSize="xl">{index}. {rest.restName}</Heading>
+            <HStack>
+              {/*{[...Array(rest.restReviews)].map((star) => {*/}
+              {/*  return <FaPaw color="7986e6"/>*/}
+              {/*})}*/}
+
+              {rest.restRating >=1 && <FaPaw color="7986e6"/>}
+              {rest.restRating < 1 && <FaPaw color="gray"/>}
+              {rest.restRating >=2 && <FaPaw color="7986e6"/>}
+              {rest.restRating < 2 && <FaPaw color="gray"/>}
+              {rest.restRating >=3 && <FaPaw color="7986e6"/>}
+              {rest.restRating < 3 && <FaPaw color="gray"/>}
+              {rest.restRating >=4 && <FaPaw color="7986e6"/>}
+              {rest.restRating < 4 && <FaPaw color="gray"/>}
+              {rest.restRating >=5 && <FaPaw color="7986e6"/>}
+              {rest.restRating < 5 && <FaPaw color="gray"/>}
+
+              <Text fontSize="12" fontWeight="bold">{rest.restReviews} Reviews</Text>
+              <Text fontSize="12">({rest.studentRev} student reviews & {rest.otherRev} other reviews)</Text>
+            </HStack>
+            <Text>{rest.restDes}</Text>
+            <hr/>
+            <HStack>
+              {rest.husky && <FaCheck fontSize="12" color="green"/>}
+              {!rest.husky && <FaTimes fontSize="12" color="red"/>}
+              <Text fontSize="12">Husky Dollars</Text>
+
+              {rest.discount && <FaCheck fontSize="12" color="green"/>}
+              {!rest.discount && <FaTimes fontSize="12" color="red"/>}
+              <Text fontSize="12">Student Discount</Text>
+            </HStack>
+
+            <HStack>
+            {rest.tags.map((tags) => {
+              return <Box fontSize="12" borderRadius="md" background="#7986e6" color="white" p="0.5">{tags}</Box>
+            })}
+            </HStack>
+
+          </VStack>
+        </HStack>
+      </Box>
+  );
+}
+export default SearchItem;
