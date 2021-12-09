b3f9549dfdd43a748b6dc519fc38f0ac4f5a5e1d
nguy3n.nhat <nguy3n.nhat@gmail.com>
2021-12-08 21:14:39 -0500

    fix search params

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Box,
   Button,
@@ -7,43 +7,81 @@ import {
   IconButton,
 } from "@chakra-ui/react";
 import {MdFastfood} from "react-icons/all";
-import {Outlet} from "react-router-dom";
-import {Link as ReachLink} from "react-router-dom";
-import {useAuth0} from "@auth0/auth0-react";
+import { Outlet } from "react-router-dom";
+import { Link as ReachLink } from "react-router-dom";
+
+import { API_URL } from "../../consts";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-  const {loginWithRedirect, isAuthenticated, logout} = useAuth0();
-  let button;
-  if (!isAuthenticated) {
-    button =
-        <Button m={3} onClick={() => loginWithRedirect()}>
-          <ReachLink to="/login">Login</ReachLink>
-        </Button>;
-  } else {
-    button =
-        <Button m={3}
-                onClick={() => logout({returnTo: window.location.origin})}>
-          <ReachLink to="/login">Logout</ReachLink>
-        </Button>
+  const navigate = useNavigate();
+  const [user, setUser] = useState({});
+  const [auth, setAuth] = useState({});
+  const getProfile = () => {
+    fetch(`${API_URL}/profile`, {
+      method: 'POST',
+      credentials: 'include'
+    }).then(res => res.json())
+    .then(user => {
+      setUser(user);
+    })
   }
-  return (
+
+  const getAuth = () => {
+    fetch(`${API_URL}/auth`, {
+      method: 'POST',
+      credentials: 'include'
+    }).then(res => res.json())
+    .then(auth => {
+      setAuth(auth);
+    })
+  }
+  useEffect(getAuth, [navigate]);
+
+  const logout = () => {
+    fetch(`${API_URL}/logout`, {
+      method: 'POST',
+      credentials: 'include'
+    }).then(res => navigate(''))
+    .then(res => {
+      window.location.reload();
+    });
+  }
+
+  console.log(auth);
+  useEffect(getProfile, [navigate]);
+  console.log(user);
+
+
+  let button;
+  let register;
+  if (typeof(auth) == "boolean") {
+    button = <Button m={3} onClick={ logout }>
+      Logout
+    </Button>
+    register = <Button mr={3} bg="#7986e6" color="white"><ReachLink to="/profile">Profile</ReachLink></Button>
+  }
+  else {
+    button = <Button m={3}>
+      <ReachLink to="/login">Login</ReachLink>
+    </Button>
+    register = <Button mr={3} bg="#7986e6" color="white"><ReachLink to="/register">Register</ReachLink></Button>
+  }
+  return(
       <>
         <Flex>
           <Box p="3">
-            <IconButton as={ReachLink} to="/" icon={<MdFastfood size={25}/>}
-                        w="50px" h="50px" p={3} bg="#7986e6" borderRadius="10px"
-                        color="white"/>
+            <IconButton as={ReachLink} to="/" icon={<MdFastfood size={25}/>} w="50px" h="50px" p={3} bg="#7986e6" borderRadius="10px" color="white"/>
             {/*<Heading size="md">Chakra App</Heading>*/}
           </Box>
-          <Spacer/>
+          <Spacer />
           <Box>
             <ReachLink to="/privacy">Privacy Policy</ReachLink>
             {button}
-            <Button mr={3} bg="#7986e6" color="white"><ReachLink
-                to="/register">Register</ReachLink></Button>
+            {register}
           </Box>
         </Flex>
-        <Outlet/>
+        <Outlet />
       </>
   );
 
