077d9974179c2fbaef0361096de92e7924d11950
nguy3n.nhat <nguy3n.nhat@gmail.com>
2021-12-08 21:10:02 -0500

    fix search params

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,41 +7,44 @@ import {
   IconButton,
 } from "@chakra-ui/react";
 import {MdFastfood} from "react-icons/all";
-import { Outlet } from "react-router-dom";
-import { Link as ReachLink } from "react-router-dom";
-import { useAuth0 } from "@auth0/auth0-react";
+import {Outlet} from "react-router-dom";
+import {Link as ReachLink} from "react-router-dom";
+import {useAuth0} from "@auth0/auth0-react";
 
 const Header = () => {
-  const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
+  const {loginWithRedirect, isAuthenticated, logout} = useAuth0();
   let button;
   if (!isAuthenticated) {
-    button = 
-      <Button m={3} onClick={() => loginWithRedirect()}>
-        <ReachLink to="/login">Login</ReachLink>
-      </Button>;
-  }
-  else {
     button =
-      <Button m={3} onClick={() => logout({ returnTo: window.location.origin })}>
-        <ReachLink to="/login">Logout</ReachLink>
-      </Button>
+        <Button m={3} onClick={() => loginWithRedirect()}>
+          <ReachLink to="/login">Login</ReachLink>
+        </Button>;
+  } else {
+    button =
+        <Button m={3}
+                onClick={() => logout({returnTo: window.location.origin})}>
+          <ReachLink to="/login">Logout</ReachLink>
+        </Button>
   }
-  return(
-    <>
-      <Flex>
-        <Box p="3">
-        <IconButton as={ReachLink} to="/" icon={<MdFastfood size={25}/>} w="50px" h="50px" p={3} bg="#7986e6" borderRadius="10px" color="white"/>
-          {/*<Heading size="md">Chakra App</Heading>*/}
-        </Box>
-        <Spacer />
-        <Box>
-          <ReachLink to="/privacy">Privacy Policy</ReachLink>
-          {button}
-          <Button mr={3} bg="#7986e6" color="white"><ReachLink to="/register">Register</ReachLink></Button>
-        </Box>
-      </Flex>
-      <Outlet />
-  </>
+  return (
+      <>
+        <Flex>
+          <Box p="3">
+            <IconButton as={ReachLink} to="/" icon={<MdFastfood size={25}/>}
+                        w="50px" h="50px" p={3} bg="#7986e6" borderRadius="10px"
+                        color="white"/>
+            {/*<Heading size="md">Chakra App</Heading>*/}
+          </Box>
+          <Spacer/>
+          <Box>
+            <ReachLink to="/privacy">Privacy Policy</ReachLink>
+            {button}
+            <Button mr={3} bg="#7986e6" color="white"><ReachLink
+                to="/register">Register</ReachLink></Button>
+          </Box>
+        </Flex>
+        <Outlet/>
+      </>
   );
 
 };
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -2,23 +2,19 @@ import React, {useEffect, useState} from "react";
 import {Box, Button, HStack, Input, Text, VStack} from "@chakra-ui/react";
 
 import SearchItems from "./SearchItems";
-import {useNavigate, Link, useParams} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 const Search = () => {
   const Documenu = require('documenu')
   Documenu.configure('22ea8db636a5fc9b62005e445f894b22')
-  const params = useParams();
-  const restaurantName = params.restaurantTerm || 'thai'
 
-  const maxDist = params.distanceTerm
   const [restaurantTerm, setRestaurantTerm] = useState('');
-  const [distanceTerm, setDistanceTerm] = useState(maxDist);
   const [restaurant, setRestaurant] = useState([]);
 
   const findRestaurant = () => {
 
     fetch(
-        `https://api.documenu.com/v2/restaurants/search/geo?cuisine=${restaurantTerm}&lat=42.358990&lon=-71.058632&distance=${distanceTerm}&key=22ea8db636a5fc9b62005e445f894b22`)
+        `https://api.documenu.com/v2/restaurants/search/geo?cuisine=${restaurantTerm}&lat=42.358990&lon=-71.058632&distance=1&key=22ea8db636a5fc9b62005e445f894b22`)
     .then(res => res.json())
     .then(results => setRestaurant(results.data))
   }
@@ -29,48 +25,34 @@ const Search = () => {
 
       <VStack>
         <HStack>
-          <Text>Cuisine</Text>
+          <Text><b>Cuisine: </b></Text>
           <Input onChange={(e) =>
-              setRestaurantTerm(e.target.value)} />
-          <Text>Distance</Text>
-          <Input onChange={(e) =>
-              setDistanceTerm(e.target.value)}
-              value={distanceTerm}/>
-          <Button onClick={findRestaurant}>
+              setRestaurantTerm(e.target.value)}
+                 placeholder={'What do you want?'}/>
+          <Button onClick={findRestaurant} bg="#7986e6" color="white">
             Search
           </Button>
         </HStack>
-
         <Box p="2" w="80%">
-          <Text fontWeight="bold" fontSize="2em">Results
-            for {restaurantTerm}...</Text>
-
-            {
-
-              restaurant.map((restaurant, index) => {
-                return (
-                    <Box key={restaurant.restaurant_id}>
-                      <Link to={`/details/${restaurant.restaurant_id}`}>
-                        <VStack spacing="12px">
-                          <Box/>
-                          <SearchItems restaurant={restaurant}
-                                       index={index + 1}/>
-                        </VStack>
-                      </Link>
-                    </Box>
-                );
-              })
-
-            }
-
-
-
+            <Text fontWeight="bold" fontSize="2em">Search results for {restaurantTerm}...</Text>
+          {
+            restaurant.map((restaurant, index) => {
+              return (
+                  <Box key={restaurant.restaurant_id}>
+                    <Link to={`/details/${restaurant.restaurant_id}`}>
+                      <VStack spacing="12px">
+                        <Box/>
+                        <SearchItems restaurant={restaurant}
+                                     index={index + 1}/>
+                      </VStack>
+                    </Link>
+                  </Box>
+              );
+            })
+          }
         </Box>
-
       </VStack>
-
   );
-
 };
 
 export default Search;
