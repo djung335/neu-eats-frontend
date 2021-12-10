55c6d6711fcf9057a73db6f4fb1f5ad945052b20
folaroma <dkim1106@gmail.com>
2021-12-10 16:06:57 -0500

    commenting out trending

--- src/components/Home/Trending.js
+++ src/components/Home/Trending.js
@@ -7,7 +7,7 @@ import {Link} from "react-router-dom";
 const Trending = () => {
     return(<Box w="50%" p={3}>
     <Text fontWeight="700" textAlign="left">Trending</Text>
-    {
+    {/*{
 
 restaurant.map((restaurant, index) => {
   return (
@@ -21,7 +21,7 @@ restaurant.map((restaurant, index) => {
   );
 })
 
-}
+}*/}
   </Box>); 
     
 };
