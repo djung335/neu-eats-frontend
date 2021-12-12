e4965a7f1b70b3aa249e98b844afd1cc69525909
Daniel Jung <danieljung335@gmail.com>
2021-12-12 02:42:48 -0500

    fixes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -14,59 +14,59 @@ import { API_URL } from "../../consts";
 import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-  // const navigate = useNavigate();
-  // const [user, setUser] = useState({});
-  // const [auth, setAuth] = useState();
-  // const getProfile = () => {
-  //   fetch(`${API_URL}/profile`, {
-  //     method: 'POST',
-  //     credentials: 'include'
-  //   }).then(res => res.json())
-  //   .then(user => {
-  //     setUser(user);
-  //   })
-  // }
+  const navigate = useNavigate();
+  const [user, setUser] = useState({});
+  const [auth, setAuth] = useState();
+  const getProfile = () => {
+    fetch(`${API_URL}/profile`, {
+      method: 'POST',
+      credentials: 'include'
+    }).then(res => res.json())
+    .then(user => {
+      setUser(user);
+    })
+  }
 
-  // const getAuth = () => {
-  //   fetch(`${API_URL}/auth`, {
-  //     method: 'POST',
-  //     credentials: 'include'
-  //   }).then(res => res.json())
-  //   .then(auth => {
-  //     setAuth(auth);
-  //   })
-  // }
-  // useEffect(getAuth, [navigate]);
+  const getAuth = () => {
+    fetch(`${API_URL}/auth`, {
+      method: 'POST',
+      credentials: 'include'
+    }).then(res => res.json())
+    .then(auth => {
+      setAuth(auth);
+    })
+  }
+  useEffect(getAuth, [navigate]);
 
-  // const logout = () => {
-  //   fetch(`${API_URL}/logout`, {
-  //     method: 'POST',
-  //     credentials: 'include'
-  //   }).then(res => navigate(''))
-  //   .then(res => {
-  //     window.location.reload();
-  //   });
-  // }
+  const logout = () => {
+    fetch(`${API_URL}/logout`, {
+      method: 'POST',
+      credentials: 'include'
+    }).then(res => navigate(''))
+    .then(res => {
+      window.location.reload();
+    });
+  }
 
-  // console.log(auth);
-  // useEffect(getProfile, [navigate]);
-  // console.log(user);
+  console.log(auth);
+  useEffect(getProfile, [navigate]);
+  console.log(user);
 
 
   let button;
   let register;
-  // if (typeof(auth) == "boolean") {
-  //   button = <Button m={3} onClick={ logout }>
-  //     Logout
-  //   </Button>
-  //   register = <Button mr={3} bg="#7986e6" color="white"><ReachLink to="/profile">Profile</ReachLink></Button>
-  // }
-  // else {
-  //   button = <Button m={3}>
-  //     <ReachLink to="/login">Login</ReachLink>
-  //   </Button>
-  //   register = <Button mr={3} bg="#7986e6" color="white"><ReachLink to="/register">Register</ReachLink></Button>
-  // }
+  if (typeof(auth) == "boolean") {
+    button = <Button m={3} onClick={ logout }>
+      Logout
+    </Button>
+    register = <Button mr={3} bg="#7986e6" color="white"><ReachLink to="/profile">Profile</ReachLink></Button>
+  }
+  else {
+    button = <Button m={3}>
+      <ReachLink to="/login">Login</ReachLink>
+    </Button>
+    register = <Button mr={3} bg="#7986e6" color="white"><ReachLink to="/register">Register</ReachLink></Button>
+  }
   return(
       <>
         <Flex>
