c2c19cea31e1e33a1b43c83e47800117416a4d86
Daniel Jung <danieljung335@gmail.com>
2021-12-13 15:53:39 -0800

    added index

--- src/components/Home/Trending.js
+++ src/components/Home/Trending.js
@@ -9,7 +9,7 @@ const Trending = () => {
     return(<Box w="50%" p={3}>
     <Text fontWeight="700" textAlign="left">Trending</Text>
     {
-              restaurant.map((restaurant, index) => {
+              restaurant.map((restaurant, index={}) => {
                 return (
                     <Box key={restaurant.restaurant_id}>
                       <Link to={`/details/${restaurant.restaurant_id}`}>
