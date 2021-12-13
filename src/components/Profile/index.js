5f3272bc7132ed9971a8b0b991b776ef4d6260e5
nguy3n-nhat <42819830+nguy3n-nhat@users.noreply.github.com>
2021-12-12 21:10:10 -0500

    Update index.js

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,10 +1,19 @@
 import React from "react";
-import {Box, Flex, Text} from "@chakra-ui/react";
-
+import {
+  Box,
+  Flex,
+  Text,
+  VStack,
+  Image,
+  Stack,
+  Heading,
+  useColorModeValue
+} from "@chakra-ui/react";
+import dave from "../../images/dave.jpeg";
 import {useEffect, useState} from "react";
 import {API_URL} from "../../consts";
 import {useNavigate} from "react-router-dom";
-import ProfileCard from "./ProfileCard"
+import ProfileEdit from "./ProfileEdit"
 
 const Profile = () => {
   const [user, setUser] = useState({});
@@ -32,7 +41,42 @@ const Profile = () => {
 
         </Box>
         <Box w='40%'>
-          <ProfileCard/>
+          <VStack>
+
+            <Box
+                maxW={'270px'}
+                w={'full'}
+                bg={useColorModeValue('white', 'gray.800')}
+                boxShadow={'2xl'}
+                rounded={'md'}
+                overflow={'hidden'}>
+              <Box padding={'10px'}>
+                <Image
+                    h={'75%'}
+                    w={'full'}
+                    src={dave}
+                    objectFit={'cover'}
+                />
+              </Box>
+
+              <Box p={6}>
+                <Stack spacing={0} align={'center'} mb={5}>
+                  <Heading fontSize={'18px'} fontStyle={'Bold'}>
+                    {user.firstName} {user.lastName}
+                  </Heading>
+                </Stack>
+
+                <Text fontSize={'12px'} paddingTop={'5px'}>
+                  <b>Email:</b> {user.email}
+                </Text>
+
+
+              </Box>
+              <Box align={'right'}>
+                <ProfileEdit/>
+              </Box>
+            </Box>
+          </VStack>
         </Box>
 
 
