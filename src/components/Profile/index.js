4eeaee36cded58b109a1ef9d6f279ea30045bef5
nguy3n-nhat <42819830+nguy3n-nhat@users.noreply.github.com>
2021-12-12 20:48:18 -0500

    Update index.js

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -4,7 +4,7 @@ import {Box, Flex, Text} from "@chakra-ui/react";
 import {useEffect, useState} from "react";
 import {API_URL} from "../../consts";
 import {useNavigate} from "react-router-dom";
-
+import ProfileCard from "./ProfileCard"
 
 const Profile = () => {
   const [user, setUser] = useState({});
@@ -17,33 +17,27 @@ const Profile = () => {
     .then(user => {
       setUser(user);
     }).catch(e => navigate('/login'));
+  }
 
-  /*.catch(e => navigate('/login'))*/
-  }
-  const logout = () => {
-    fetch(`${API_URL}/logout`, {
-      method: 'POST',
-      credentials: 'include'
-    }).then(res => navigate('/'));
-  }
-  useEffect(getProfile, [navigate]);
-  return(
-      <div>
-        <h1>Profile</h1>
-        <input
-            value={user.username}
-            onChange={(e) => setUser({...user, username: e.target.value})}
-            placeholder="username"
-            className="form-control"/>
-        <button
-            onClick={logout}
-            className="btn btn-danger">
-          Logout
-        </button>
-       
-      </div>
+  return (
+      <Flex>
+        <Box w='60%'>
+          <h1>Profile</h1>
+          <h1>Reviews would go here?</h1>
+          <input
+              value={user.username}
+              onChange={(e) => setUser({...user, username: e.target.value})}
+              placeholder="username"
+              className="form-control"/>
+
+        </Box>
+        <Box w='40%'>
+          <ProfileCard/>
+        </Box>
+
+
+      </Flex>
   );
-
 
 };
 
