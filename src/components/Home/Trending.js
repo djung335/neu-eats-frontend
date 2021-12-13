04a746c66b60f6999118452952875fed5b861850
Daniel Jung <danieljung335@gmail.com>
2021-12-13 15:49:22 -0800

    home page

--- src/components/Home/Trending.js
+++ src/components/Home/Trending.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {Box, Text, VStack} from "@chakra-ui/react";
-import restaurant from "../../reducers/data/restaurants.json"
+import restaurant from "./restaurant.json"
 import SearchItems from "../Search/SearchItems";
 import {Link} from "react-router-dom";
 
@@ -8,7 +8,7 @@ const Trending = () => {
 
     return(<Box w="50%" p={3}>
     <Text fontWeight="700" textAlign="left">Trending</Text>
-    {/* {
+    {
               restaurant.map((restaurant, index) => {
                 return (
                     <Box key={restaurant.restaurant_id}>
@@ -22,7 +22,7 @@ const Trending = () => {
                     </Box>
                 );
               })
-            } */}
+            }
   </Box>); 
     
 };
