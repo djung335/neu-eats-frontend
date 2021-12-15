918e50c8d96bf53c42d33af3d3c6c3ec4a522d61
nguy3n-nhat <42819830+nguy3n-nhat@users.noreply.github.com>
2021-12-15 14:26:42 -0500

    Add files via upload

---
+++ src/components/Profile/ProfileAnon.js
@@ -0,0 +1,92 @@
+import React from "react";
+import {
+  Box,
+  Flex,
+  Text,
+  VStack,
+  HStack,
+  Image,
+  Stack,
+  Heading,
+  useColorModeValue
+} from "@chakra-ui/react";
+import dave from "../../images/dave.jpeg";
+import {useEffect, useState} from "react";
+import {API_URL} from "../../consts";
+import {useNavigate, useParams} from "react-router-dom";
+import ProfileEdit from "./ProfileEdit"
+import ProfileReviews from "./ProfileReviews"
+
+const ProfileAnon = () => {
+  const params = useParams();
+  const [user, setUser] = useState('');
+  const navigate = useNavigate();
+  const getProfile = () => {
+    fetch(`${API_URL}/users/?id=${params.id}`, {
+      method: 'GET',
+      credentials: 'include'
+    }).then(res => res.json())
+    .then(user => {
+      setUser(user);
+    }).catch(e => navigate('/login'));
+  }
+  useEffect(getProfile, [navigate]);
+
+  return (
+      <Flex>
+        <HStack>
+          <Box w='60%'>
+            <VStack align={'left'}>
+              <Heading>{user.firstName} {user.lastName}'s Reviews</Heading>
+              <ProfileReviews user={user}/>
+            </VStack>
+          </Box>
+          <Box w='40%'>
+            <VStack>
+              <Box
+                  maxW={'270px'}
+                  w={'full'}
+                  bg={useColorModeValue('white', 'gray.800')}
+                  boxShadow={'2xl'}
+                  rounded={'md'}
+                  overflow={'hidden'}>
+                <Box padding={'10px'}>
+                  <Image
+                      h={'75%'}
+                      w={'full'}
+                      src={dave}
+                      objectFit={'cover'}
+                  />
+                </Box>
+                <Box p={6}>
+                  <Stack spacing={0} align={'center'} mb={5}>
+                    <VStack>
+
+                      {user.owner && <Heading fontSize={'18px'}
+                                              fontStyle={'Bold'}>
+                        {user.firstName} {user.lastName} (Owner)
+                      </Heading>}
+                      {!user.owner && <Heading fontSize={'18px'}
+                                               fontStyle={'Bold'}>
+                        {user.firstName} {user.lastName}
+                      </Heading>}
+                      <Text fontSize={'12px'} paddingTop={'5px'}>
+                        <b>Username:</b> {user.username}
+                      </Text>
+                      <Text fontSize={'12px'} paddingTop={'5px'}>
+                        <b>Email:</b> {user.email}
+                      </Text>
+                    </VStack>
+                  </Stack>
+                </Box>
+                <Box align={'right'}>
+                  <ProfileEdit/>
+                </Box>
+              </Box>
+            </VStack>
+          </Box>
+        </HStack>
+      </Flex>
+  );
+}
+export default ProfileAnon;
