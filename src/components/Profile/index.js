92aa32a0f440c9348f1163c9638c2cec0321176b
nguy3n-nhat <42819830+nguy3n-nhat@users.noreply.github.com>
2021-12-15 14:30:06 -0500

    Update index.js

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,45 +1,93 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import {
-  Center,
+  Box,
+  Flex,
+  Text,
   VStack,
+  HStack,
+  Image,
+  Stack,
+  Heading,
+  useColorModeValue
 } from "@chakra-ui/react";
-
-import {useNavigate} from "react-router-dom";
+import dave from "../../images/dave.jpeg";
+import {useEffect, useState} from "react";
 import {API_URL} from "../../consts";
-import {Provider} from "react-redux";
-import {createStore} from "redux";
-import reviews from "../../reducers/reviews";
-import ProfileMember from "./ProfileMember";
-import ProfileAnon from "./ProfileAnon";
-
-const store = createStore(reviews);
+import {useNavigate, useParams} from "react-router-dom";
+import ProfileEdit from "./ProfileEdit"
+import ProfileReviews from "./ProfileReviews"
 
 const Profile = () => {
+  const params = useParams();
+  const [user, setUser] = useState('');
   const navigate = useNavigate();
-  const [auth, setAuth] = useState({});
-
-  const getAuth = () => {
-    fetch(`${API_URL}/auth`, {
+  const getProfile = () => {
+    fetch(`${API_URL}/profile?id=${params.id}`, {
       method: 'POST',
       credentials: 'include'
     }).then(res => res.json())
-    .then(auth => {
-      setAuth(auth);
-    })
+    .then(user => {
+      setUser(user);
+    }).catch(e => navigate('/login'));
   }
-  useEffect(getAuth, [navigate]);
 
-  let reviews;
-  if (typeof (auth) == "boolean") {
-    reviews = <ProfileAnon/>
-  } else {
-    reviews = <ProfileMember/>
-  }
+  useEffect(getProfile, [navigate]);
+
   return (
-      <Provider store={store}>
-        {reviews}
-      </Provider>
-  );
-};
+      <Flex>
+        <HStack>
+          <Box w='60%'>
+            <VStack align={'left'}>
+              <Heading>{user.firstName} {user.lastName}'s Reviews</Heading>
+              <ProfileReviews user={user}/>
+            </VStack>
+          </Box>
+          <Box w='40%'>
+            <VStack>
+              <Box
+                  maxW={'270px'}
+                  w={'full'}
+                  bg={useColorModeValue('white', 'gray.800')}
+                  boxShadow={'2xl'}
+                  rounded={'md'}
+                  overflow={'hidden'}>
+                <Box padding={'10px'}>
+                  <Image
+                      h={'75%'}
+                      w={'full'}
+                      src={dave}
+                      objectFit={'cover'}
+                  />
+                </Box>
+                <Box p={6}>
+                  <Stack spacing={0} align={'center'} mb={5}>
+                    <VStack>
 
+                      {user.owner && <Heading fontSize={'18px'}
+                                              fontStyle={'Bold'}>
+                        {user.firstName} {user.lastName} (Owner)
+                      </Heading>}
+                      {!user.owner && <Heading fontSize={'18px'}
+                                               fontStyle={'Bold'}>
+                        {user.firstName} {user.lastName}
+                      </Heading>}
+                      <Text fontSize={'12px'} paddingTop={'5px'}>
+                        <b>Username:</b> {user.username}
+                      </Text>
+                      <Text fontSize={'12px'} paddingTop={'5px'}>
+                        <b>Email:</b> {user.email}
+                      </Text>
+                    </VStack>
+                  </Stack>
+                </Box>
+                <Box align={'right'}>
+                  <ProfileEdit/>
+                </Box>
+              </Box>
+            </VStack>
+          </Box>
+        </HStack>
+      </Flex>
+  );
+}
 export default Profile;
