f0e67a0a5fd5e87ddad7f3d235f07a8f1aebf344
Daniel Jung <danieljung335@gmail.com>
2021-12-15 23:36:36 -0800

    please work

--- src/components/Home/PopularReviews.js
+++ src/components/Home/PopularReviews.js
@@ -1,19 +1,52 @@
-import React from "react";
+import React, {useState, useNavigate, useEffect} from "react";
 import {Box, Text, Wrap} from "@chakra-ui/react";
 import reviews from "./reviews.json";
+import reviews2 from "./reviews2.json";
 import PopularReviewItem from "./PopularReviewItem";
+import { API_URL } from "../../consts";
 
 const PopularReviews = () => {
+  const [auth, setAuth] = useState();
+  const navigate = useNavigate();
+
+  const getAuth = () => {
+    fetch(`${API_URL}/auth`, {
+      method: 'POST',
+      credentials: 'include'
+    }).then(res => res.json())
+    .then(auth => {
+      setAuth(auth);
+    })
+  }
+  useEffect(getAuth, [navigate]);
+
+  if (typeof(auth) == "boolean") {
     return(     
-    <Box w="50%" p={3}>
-      <Text md={5} fontWeight="700" textAlign="left">Popular Reviews</Text>
-      <Wrap spacing='25px'>
-        {
-          reviews.map((review) => <PopularReviewItem review={review} />)
-        }
-      </Wrap>
-    </Box>
-    );
+      <Box w="50%" p={3}>
+        <Text md={5} fontWeight="700" textAlign="left">My Reviews</Text>
+        <Wrap spacing='25px'>
+          {
+            reviews2.map((review) => <PopularReviewItem review={review} />)
+          }
+        </Wrap>
+      </Box>
+      );
+    
+  } else {
+    return(     
+      <Box w="50%" p={3}>
+        <Text md={5} fontWeight="700" textAlign="left">Popular Reviews</Text>
+        <Wrap spacing='25px'>
+          {
+            reviews.map((review) => <PopularReviewItem review={review} />)
+          }
+        </Wrap>
+      </Box>
+      );
+
+  }
+
+
 };
 
 export default PopularReviews;
--- src/components/Home/Trending.js
+++ src/components/Home/Trending.js
@@ -1,11 +1,45 @@
-import React from "react";
+import React, {useState, useNavigate, useEffect} from "react";
 import {Box, Text, VStack} from "@chakra-ui/react";
 import restaurant from "./restaurant.json"
+import restaurant2 from "./restaurant2.json"
 import SearchItems from "../Search/SearchItems";
 import {Link} from "react-router-dom";
+import { API_URL } from "../../consts";
 
 const Trending = () => {
+  const [auth, setAuth] = useState();
+  const navigate = useNavigate();
 
+  const getAuth = () => {
+    fetch(`${API_URL}/auth`, {
+      method: 'POST',
+      credentials: 'include'
+    }).then(res => res.json())
+    .then(auth => {
+      setAuth(auth);
+    })
+  }
+  useEffect(getAuth, [navigate]);
+  if (typeof(auth) == "boolean") {
+    return(<Box w="50%" p={3}>
+    <Text md={5} fontWeight="700" textAlign="left">Favorite Restaurants</Text>
+    {
+              restaurant2.map((restaurant, index) => {
+                return (
+                    <Box key={restaurant.restaurant_id}>
+                      <Link to={`/details/${restaurant.restaurant_id}`}>
+                        {/* <VStack spacing="12px"> */}
+                          <Box/>
+                          <SearchItems restaurant={restaurant}
+                                       index={index + 1}/>
+                        {/* </VStack> */}
+                      </Link>
+                    </Box>
+                );
+              })
+            }
+  </Box>); 
+  } else {
     return(<Box w="50%" p={3}>
     <Text md={5} fontWeight="700" textAlign="left">Trending</Text>
     {
@@ -24,6 +58,8 @@ const Trending = () => {
               })
             }
   </Box>); 
+  }
+
     
 };
 
