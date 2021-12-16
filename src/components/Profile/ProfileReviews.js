c6af794180d496ff381006c09cdabbd780b45189
nguy3n-nhat <42819830+nguy3n-nhat@users.noreply.github.com>
2021-12-16 10:14:51 -0500

    Update ProfileReviews.js

--- src/components/Profile/ProfileReviews.js
+++ src/components/Profile/ProfileReviews.js
@@ -9,15 +9,7 @@ import React from "react";
 import ProfileReviewItem from "./ProfileReviewItem";
 import reviews from "./reviews.json";
 
-const ProfileReviews = ({user = {name: "Dave peters",
-  user: true,
-  userName: "dave24",
-  restaurant: "Qdoba",
-  email: "qdoba.com",
-  phoneNumber: "(123)-456-7890",
-  reviews: "2",
-  friends: "10",
-  bio: "Ut enim ad minim veniam, quis nostrud exercitation"}}) => {
+const ProfileReviews = () => {
   return(
       <>
         {
